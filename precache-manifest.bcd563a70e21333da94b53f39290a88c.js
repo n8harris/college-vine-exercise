f264b57a60392882db352b850d94013e1ac557b8
Nathan Harris <nate@natepharris.me>
2018-10-17 19:58:41 -0600

    Updates

---
+++ precache-manifest.bcd563a70e21333da94b53f39290a88c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a57a8d63f5e08a3308e8",
+    "url": "/college-vine-exercise/static/js/runtime~main.a57a8d63.js"
+  },
+  {
+    "revision": "0b34b895beb57c384014",
+    "url": "/college-vine-exercise/static/js/main.0b34b895.chunk.js"
+  },
+  {
+    "revision": "46e04b11e479a72b6f8e",
+    "url": "/college-vine-exercise/static/js/1.46e04b11.chunk.js"
+  },
+  {
+    "revision": "0b34b895beb57c384014",
+    "url": "/college-vine-exercise/static/css/main.5ca85bef.chunk.css"
+  },
+  {
+    "revision": "b7cffca45166cec5a16a281c30a9fb2d",
+    "url": "/college-vine-exercise/index.html"
+  }
+];
